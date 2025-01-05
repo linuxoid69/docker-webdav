61583e528fcacb07722d2051bc564276b5c8e0f9
Rustam Tagaev <rustam.tagaev@avroid.tech>
2025-01-05 12:09:31 +0300

    []  rename dir for configs

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -7,7 +7,7 @@ WEBDAV_SYMLINK_ENABLE=${WEBDAV_SYMLINK_ENABLE:-''}
 if [ "${WEBDAV_USERNAME}" != "" ] && [ "${WEBDAV_PASSWORD}" != "" ]
 then
         sed -i -r 's/(server_name.*)/\1\nauth_basic "Upload directory";\nauth_basic_user_file \/var\/lib\/nginx\/.htpasswd;/' \
-            /etc/nginx/conf.d/default.conf
+            /etc/nginx/http.d/default.conf
         htpasswd -dbcm /var/lib/nginx/.htpasswd "${WEBDAV_USERNAME}" "${WEBDAV_PASSWORD}"
 elif [ "${WEBDAV_USERNAME}" != "" ] || [ "${WEBDAV_PASSWORD}" != "" ]
 then
@@ -15,10 +15,10 @@ then
     exit 123
 fi
 
-[ "${WEBDAV_SYMLINK_ENABLE}" = "yes" ] && echo 'disable_symlinks off;' >> /etc/nginx/conf.d/http_parameters.conf
+[ "${WEBDAV_SYMLINK_ENABLE}" = "yes" ] && echo 'disable_symlinks off;' >> /etc/nginx/http.d/http_parameters.conf
 
 WEBDAV_PORT=${WEBDAV_PORT:-8080}
 
-sed -i -r "s/listen .*;/listen ${WEBDAV_PORT};/" /etc/nginx/conf.d/default.conf
+sed -i -r "s/listen .*;/listen ${WEBDAV_PORT};/" /etc/nginx/http.d/default.conf
 
 nginx -g "daemon off;"
