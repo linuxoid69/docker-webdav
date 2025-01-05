c3adfe6d455e7af8a23028190a128c5587f377e5
Rustam Tagaev <rustam.tagaev@avroid.tech>
2025-01-05 12:47:38 +0300

    []  change condition and add include vault secrets

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -4,12 +4,17 @@ WEBDAV_USERNAME=${WEBDAV_USERNAME:-''}
 WEBDAV_PASSWORD=${WEBDAV_PASSWORD:-''}
 WEBDAV_SYMLINK_ENABLE=${WEBDAV_SYMLINK_ENABLE:-''}
 
+if [ -f /vault/secrets/webdav ]
+then
+    . /vault/secrets/webdav
+fi
+
 if [ "${WEBDAV_USERNAME}" != "" ] && [ "${WEBDAV_PASSWORD}" != "" ]
 then
         sed -i -r 's/(server_name.*)/\1\nauth_basic "Upload directory";\nauth_basic_user_file \/var\/lib\/nginx\/.htpasswd;/' \
             /etc/nginx/http.d/default.conf
         htpasswd -dbcm /var/lib/nginx/.htpasswd "${WEBDAV_USERNAME}" "${WEBDAV_PASSWORD}"
-elif [ "${WEBDAV_USERNAME}" != "" ] || [ "${WEBDAV_PASSWORD}" != "" ]
+elif [ "${WEBDAV_USERNAME}" = "" ] || [ "${WEBDAV_PASSWORD}" = "" ]
 then
     echo "WEBDAV_USERNAME and WEBDAV_PASSWORD must be set"
     exit 123
