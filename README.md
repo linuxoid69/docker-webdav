8b38bf20341446c0d8a67bf996be7156cf180939
Rustam Tagaev <rustam.tagaev@avroid.tech>
2025-01-05 11:43:19 +0300

    []  start

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# docker-webdav
+# Webdav
+
+Команда запуска
+
+```bash
+docker run -it --rm  -p 8080:8080  -v <your_data>:/var/www/localhost/htdocs reg.netcitylife.ru/docker/webdav
+```
+
+Переменные окружения
+
+```bash
+WEBDAV_USERNAME=admin     - имя пользователя для доступа к webdav
+WEBDAV_PASSWORD=1234      - пароль
+WEBDAV_SYMLINK_ENABLE=yes - если нужно что бы nginx обрабатывал symlink
+WEBDAV_PORT=8080          - порт
+```
---
+++ rootfs/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+WEBDAV_USERNAME=${WEBDAV_USERNAME:-''}
+WEBDAV_PASSWORD=${WEBDAV_PASSWORD:-''}
+WEBDAV_SYMLINK_ENABLE=${WEBDAV_SYMLINK_ENABLE:-''}
+
+if [ "${WEBDAV_USERNAME}" != "" ] && [ "${WEBDAV_PASSWORD}" != "" ]
+then
+        sed -i -r 's/(server_name.*)/\1\nauth_basic "Upload directory";\nauth_basic_user_file \/var\/lib\/nginx\/.htpasswd;/' \
+            /etc/nginx/conf.d/default.conf
+        htpasswd -dbcm /var/lib/nginx/.htpasswd "${WEBDAV_USERNAME}" "${WEBDAV_PASSWORD}"
+elif [ "${WEBDAV_USERNAME}" != "" ] || [ "${WEBDAV_PASSWORD}" != "" ]
+then
+    echo "WEBDAV_USERNAME and WEBDAV_PASSWORD must be set"
+    exit 123
+fi
+
+[ "${WEBDAV_SYMLINK_ENABLE}" = "yes" ] && echo 'disable_symlinks off;' >> /etc/nginx/conf.d/http_parameters.conf
+
+WEBDAV_PORT=${WEBDAV_PORT:-8080}
+
+sed -i -r "s/listen .*;/listen ${WEBDAV_PORT};/" /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;"
